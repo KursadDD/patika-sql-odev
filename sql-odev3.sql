f6d9cc40ccbf56eba3f4b5c678d01569f418a419
KursadDD <75373273+KursadDD@users.noreply.github.com>
2023-04-02 13:22:23 +0300

    odev3 added

---
+++ sql-odev3.sql
@@ -0,0 +1,7 @@
+1. select country from country where country like 'A%' and country like '%a'
+
+2. select country from country where 6<= LEN(country) and country like '%n'
+
+3. select title from film where title ilike '%t%t%t%t%'
+
+4.select * from film where title like '%C%' and 90<length and rental_rate=2.99
